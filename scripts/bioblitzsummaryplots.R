13e19e27a6faf01af70b295e05323493912c9234
maggie slein <maggieslein77@gmail.com>
2023-11-30 08:11:57 -0800

    rough rmd

---
+++ scripts/bioblitzsummaryplots.R
@@ -0,0 +1,107 @@
+#reading in the total summary dataset
+arthropod <- read_csv("Galiano_terrestrial_arthropods_review_summary_2023-10-21.csv")
+#splitting into data that has data for the first observed date (musueum specimens most likely)
+first.observed <- arthropod %>%
+  drop_na(First.Observed) %>%
+  select(Taxon, First.Observed)%>%
+  mutate(full_date = First.Observed)
+#reformatting date
+first.observed$First.Observed <- format(first.observed$First.Observed, "%Y/%m")
+
+#splitting into data has data for the collected reported date (inat observations?)
+collected.reported <- arthropod %>%
+  drop_na(Collected.Reported..y.m.d.) %>%
+  select(Taxon, Collected.Reported..y.m.d.) %>%
+  rename(First.Observed = Collected.Reported..y.m.d.) %>%
+  mutate(First.Observed = as.Date(First.Observed))%>%
+  mutate(full_date = First.Observed)
+#reformatting date
+collected.reported$First.Observed <- format(collected.reported$First.Observed, "%Y/%m")
+
+#glueing these  datasets together again for the full analysis
+combined <- rbind(first.observed, collected.reported) %>% 
+  group_by(Taxon) %>% filter(First.Observed == min(First.Observed)) %>%
+  ungroup() %>%
+  mutate(date_final = format(lubridate::ymd(full_date), '%Y-%m-%d'),
+         year = lubridate::year(date_final), 
+         month = lubridate::month(date_final))
+
+
+
+#collating by year
+split_biodiv_year <- function(df, x, y) {
+  split <- df %>% filter(between(year,x,y))
+  cum.obs <- nrow(split) # cumulative no. observations
+  cum.spp <- length(unique(split$Taxon)) # cumulative no. species
+  biodiv_calcs <- data.frame(cum.obs, cum.spp) 
+  biodiv_calcs
+}
+
+y.1925.1935 <- split_biodiv(combined, 1925,1935) %>% mutate(range= "1925-1935")
+y.1925.1945 <- split_biodiv(combined, 1925,1945) %>% mutate(range= "1925-1945")
+y.1925.1955 <- split_biodiv(combined, 1925,1955) %>% mutate(range= "1925-1955")
+y.1925.1965 <- split_biodiv(combined, 1925,1965) %>% mutate(range= "1925-1965")
+y.1925.1975 <- split_biodiv(combined, 1925,1975) %>% mutate(range= "1925-1975")
+y.1925.1985 <- split_biodiv(combined, 1925,1985) %>% mutate(range= "1925-1985")
+y.1925.1995 <- split_biodiv(combined, 1925,1995) %>% mutate(range= "1925-1995")
+y.1925.2005 <- split_biodiv(combined, 1925,2005) %>% mutate(range= "1925-2005")
+y.1925.2015 <- split_biodiv(combined, 1925,2015) %>% mutate(range= "1925-2015")
+y.1925.2025 <- split_biodiv(combined, 1925,2025) %>% mutate(range= "1925-2025")
+
+
+summary_year <- rbind(y.1925.1935, y.1925.1945, y.1925.1955, y.1925.1965, 
+                      y.1925.1975, y.1925.1985, y.1925.1995, y.1925.2005, 
+                      y.1925.2025)
+
+
+#collating by month 
+split_biodiv_month <- function(df, x) {
+  split <- df %>% filter(month %in% x)
+  cum.obs <- nrow(split) # cumulative no. observations
+  cum.spp <- length(unique(split$Taxon)) # cumulative no. species
+  biodiv_calcs <- data.frame(cum.obs, cum.spp) 
+  biodiv_calcs
+}
+
+m.1 <- split_biodiv_month(combined, 1) %>% mutate(month= "1")
+m.2 <- split_biodiv_month(combined, 2) %>% mutate(month= "2")
+m.3 <- split_biodiv_month(combined, 3) %>% mutate(month= "3")
+m.4 <- split_biodiv_month(combined, 4) %>% mutate(month= "4")
+m.5 <- split_biodiv_month(combined, 5) %>% mutate(month= "5")
+m.6 <- split_biodiv_month(combined, 6) %>% mutate(month= "6")
+m.7 <- split_biodiv_month(combined, 7) %>% mutate(month= "7")
+m.8 <- split_biodiv_month(combined, 8) %>% mutate(month= "8")
+m.9 <- split_biodiv_month(combined, 9) %>% mutate(month= "9")
+m.10<- split_biodiv_month(combined, 10) %>% mutate(month= "10")
+m.11<- split_biodiv_month(combined, 11) %>% mutate(month= "11")
+m.12<- split_biodiv_month(combined, 12) %>% mutate(month= "12")
+
+summary_month <- rbind(m.1, m.2, m.3, m.4, m.5, m.6, m.7, 
+                       m.8, m.9, m.10, m.11, m.12)
+
+#focus on one figure after the next, have a separate script for each of these figs
+#context for interpretting each figure 
+#start with the years going by, then proceed to the 2023 data
+summary_monthp <- summary_month %>%
+  plot_ly(y = ~cum.spp, x=~as.numeric(month), type="scatter")
+
+
+summary_2023 <- combined %>%
+  filter(year == 2023) %>%
+  group_by(month) %>%
+  summarise(spp = length(unique(Taxon)))
+
+
+summary2023_plot<-summary_2023 %>%
+  plot_ly(y = ~spp, x=~as.numeric(month), type="scatter")
+
+
+print(summary_monthp)
+print(summary2023_plot)
+
+
+
+
+
+
+
---
+++ scripts/hfnewobs2023.R
@@ -0,0 +1,99 @@
+# Load libraries
+pacman::p_load(sf, tidyverse, sf)
+
+# Source dependencies
+
+source("scripts/utils.R")
+
+# Analysis of historical collection activities
+inat_tidy <- read_csv("inat data/intersection_inat_hunterstonmap_assoc_polygons.csv") %>%
+  filter(quality_grade == "research") #lose ~1000 rows of data
+hunterston2023 <- inat_tidy %>%
+  drop_na(Id_2)%>% #70% data loss 
+  mutate(year=2023) %>%
+  rename(type = Type, Id = Id_2, scientificName = scientific_name) %>%
+  select(scientificName, Id, latitude, longitude, year, type)
+hunterston2010 <- read_csv("inat data/hunterston2010_data+updatedpolygons.csv") %>%
+  mutate(year=2010) %>%
+  rename(scientificName = Scientific, 
+         type = Type, 
+         latitude = Latitude, 
+         longitude = Longitude) %>%
+  select(scientificName, Id, latitude, longitude, year, type)
+
+hunterston2010_names <- unique(hunterston2010$scientificName)
+hunterston2023_coords <- hunterston2023 %>% select(Id, latitude, longitude)
+confirmed_obs <- hunterston2023 %>% filter(scientificName %in% hunterston2010_names) #261 confirmed observations
+new_obs <- hunterston2023 %>% filter(!scientificName %in% hunterston2010_names) #134 new observations
+
+confirmed_cultr <- confirmed_obs %>% filter(scientificName %in% hulq.names) # 2 confirmed
+new_cult <- new_obs %>% filter(scientificName %in% hulq.names) # 2 new 
+
+summary_2010 <- hunterston2010 %>%
+  group_by(Id) %>%
+  summarise(species=length(unique(scientificName)))
+
+summary_2023 <- hunterston2023 %>%
+  group_by(Id) %>%
+  summarise(species=length(unique(scientificName)))
+
+summary_new <- new_obs %>%
+  group_by(Id) %>%
+  summarise(species=length(unique(scientificName)))
+
+summary_confirmed <- confirmed_obs %>%
+  group_by(Id) %>%
+  summarise(species=length(unique(scientificName)))
+
+# loading in polygons from shp file
+grid <- st_read("new_hunterston_map.shp") %>%
+  select(Id, geometry)
+#merging shp file with biodiversity summary
+summary_2010pn <- merge(grid, summary_2010, by="Id")
+summary_2023pn <- merge(grid, summary_2023, by="Id")
+summary_newpn <- merge(grid, summary_new, by="Id")
+summary_confirmedpn <- merge(grid, summary_confirmed, by="Id")
+
+#plotting
+
+summary_2010pnplot <- summary_2010pn %>%
+  ggplot()+
+  geom_sf(aes(fill=species))+
+  scale_fill_viridis(option="D")+
+  ggtitle("2010")
+
+summary_2023pnplot <- summary_2023pn %>%
+  ggplot()+
+  geom_sf(aes(fill=species))+
+  scale_fill_viridis(option="D")+
+  ggtitle("2023")
+
+summary_newpnplot <- summary_newpn %>%
+  ggplot()+
+  geom_sf(aes(fill=species))+
+  scale_fill_viridis(option="D")+
+  ggtitle("new species")
+
+summary_confirmedpnplot <- summary_confirmedpn %>%
+  ggplot()+
+  geom_sf(aes(fill=species))+
+  scale_fill_viridis(option="D")+
+  ggtitle("confirmed species")
+
+
+
+print(summary_2010pnplot)
+print(summary_2023pnplot)
+print(summary_newpnplot)
+print(summary_confirmedpnplot)
+
+
+
+
+
+
+
+
+
+
+
